9e6827d8e8caa9b7c97984adf8a7c2e7e839c984
oOFaYOo <nika.tseleva@mail.ru>
2021-03-19 21:14:10 +0500

    набросок разметки и стилей

---
+++ index.js
@@ -0,0 +1,5 @@
+let api = "https://api.openweathermap.org/data/2.5/" +
+    "onecall?lat=33.441792&lon=-94.037689&exclude=hourly," +
+    "current,minutely,alerts&lang=ru&units=metric&appid=6b70c540feba0fdeebeb7eb39708b7e7";
+
+
